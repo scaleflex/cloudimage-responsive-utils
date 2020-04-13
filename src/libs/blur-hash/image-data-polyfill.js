b31ce9fd5adcb7c8a596c1d9f9040a06e1de0f49
dmitry <dmitry@scaleflex.cloud>
2020-04-13 12:58:44 +0200

    feat: add support for SSR

--- src/libs/blur-hash/image-data-polyfill.js
+++ src/libs/blur-hash/image-data-polyfill.js
@@ -1,51 +1,55 @@
-if(window.CanvasPixelArray) {
-  CanvasPixelArray.prototype.set = function(arr) {
-    var l=this.length, i=0;
+if (typeof window !== 'undefined') {
+  if (window && window.CanvasPixelArray) {
+    CanvasPixelArray.prototype.set = function (arr) {
+      var l = this.length, i = 0;
 
-    for(;i<l;i++) {
-      this[i] = arr[i];
-    }
-  };
+      for (; i < l; i++) {
+        this[i] = arr[i];
+      }
+    };
+  }
 }
 (() => {
-  try {
-    new window.ImageData(new Uint8ClampedArray([0, 0, 0, 0]), 1, 1);
-  } catch (e) {
-    function ImageDataPolyfill () {
-      let args = [...arguments], data;
+  if (typeof window !== 'undefined') {
+    try {
+      new window.ImageData(new Uint8ClampedArray([0, 0, 0, 0]), 1, 1);
+    } catch (e) {
+      function ImageDataPolyfill() {
+        let args = [...arguments], data;
 
-      if (args.length < 2) {
-        throw new TypeError(`
+        if (args.length < 2) {
+          throw new TypeError(`
           Failed to construct 'ImageData': 2 arguments required, but only ${args.length} present.
         `);
-      }
+        }
 
-      if (args.length > 2) {
-        data = args.shift();
+        if (args.length > 2) {
+          data = args.shift();
 
-        if (!(data instanceof Uint8ClampedArray)) {
-          throw new TypeError(`
+          if (!(data instanceof Uint8ClampedArray)) {
+            throw new TypeError(`
             Failed to construct 'ImageData': parameter 1 is not of type 'Uint8ClampedArray'
           `);
-        }
+          }
 
-        if (data.length !== 4 * args[0] * args[1]) {
-          throw new Error(`
+          if (data.length !== 4 * args[0] * args[1]) {
+            throw new Error(`
             Failed to construct 'ImageData': The input data byte length is not a multiple of (4 * width * height)
           `);
+          }
         }
-      }
 
-      const width = args[0],
-        height = args[1],
-        canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        imageData = ctx.createImageData(width, height);
+        const width = args[0],
+          height = args[1],
+          canvas = document.createElement('canvas'),
+          ctx = canvas.getContext('2d'),
+          imageData = ctx.createImageData(width, height);
 
-      if (data) imageData.data.set(data);
-      return imageData;
-    };
+        if (data) imageData.data.set(data);
+        return imageData;
+      };
 
-    window.ImageData = ImageDataPolyfill;
+      window.ImageData = ImageDataPolyfill;
+    }
   }
 })();
