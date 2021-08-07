4ec9cf7637086404510430bfc8256cd6777eda4e
dmitry <dmitry@scaleflex.com>
2021-08-07 12:58:40 +0800

    fix: getParentContainerSize function when element was deleted in the middle of calculation

--- src/utils/get-parent-container-size.js
+++ src/utils/get-parent-container-size.js
@@ -1,15 +1,20 @@
 export const getParentContainerSize = (img, type = 'width') => {
-  let parentNode = null;
+  let parentNode = img;
   let size = 0;
+  let maxCount = 0;
 
   do {
-    parentNode = (parentNode && parentNode.parentNode) || img.parentNode;
+    parentNode = parentNode && parentNode.parentNode;
     size = typeof parentNode.getBoundingClientRect === 'function' ? parentNode.getBoundingClientRect()[type] : window.innerWidth;
-  } while (parentNode && !size)
+    maxCount = maxCount + 1;
+  } while (parentNode && !size && maxCount > 5)
 
-  const leftPadding = (size && parentNode && parentNode.nodeType === 1) ? parseInt(window.getComputedStyle(parentNode).paddingLeft) : 0
+  const leftPadding = (size && parentNode && parentNode.nodeType === 1) ? parseInt(window.getComputedStyle(parentNode).paddingLeft) : 0;
+  const rightPadding = (size && parentNode && parentNode.nodeType === 1) ? parseInt(window.getComputedStyle(parentNode).paddingRight) : 0;
 
-  const rightPadding = (size && parentNode && parentNode.nodeType === 1) ? parseInt(window.getComputedStyle(parentNode).paddingRight) : 0
+  if (!size) {
+    size = window.innerWidth;
+  }
 
   return size + (size ? (-leftPadding - rightPadding) : 0);
 };
