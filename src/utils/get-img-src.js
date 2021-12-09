c4a4f531372a33ab840ef40c5896bee8c539323a
Amr Wagdy <wagdy@scaleflex.com>
2021-12-09 16:05:25 +0200

    fix: img-src starts with // T5618

--- src/utils/get-img-src.js
+++ src/utils/get-img-src.js
@@ -6,7 +6,7 @@ export const getImgSRC = (src, baseURL = '') => {
   const relativeURLPath = isRelativeUrlPath(src);
 
   if (src.indexOf('//') === 0) {
-    src = !isServer() ? window.location.protocol : 'https:' + src;
+    src = (!isServer() ? window.location.protocol : 'https:') + src;
   }
 
   if (relativeURLPath) {
@@ -60,7 +60,7 @@ const isRelativeUrlPath = src => {
   if (!src) return false;
 
   if (src.indexOf('//') === 0) {
-    src = !isServer() ? window.location.protocol : 'https:' + src;
+    src = (!isServer() ? window.location.protocol : 'https:') + src;
   }
 
   return src.indexOf('http://') !== 0 && src.indexOf('https://') !== 0 && src.indexOf('//') !== 0;
